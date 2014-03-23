c919174cbaf660f6f9f3ba44fa1459061deb1b8b
George Field <gf12815@my.bristol.ac.uk>
2014-03-23 23:22:24 +0000

    Very basic implementation of Web API 2.

---
+++ JobServer/JobServer/Controllers/JobResultsController.cs
@@ -0,0 +1,34 @@
+﻿using JobServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace JobServer.Controllers
+{
+    public class JobResultsController : ApiController
+    {
+        private JobResult[] placeholderResults = new JobResult[]
+        {
+            new JobResult { JobId = 1, ImageA = "0deae28a26727ebe30ecf2896e5862f1", ImageB = "2d1dac96639c5e6f6246f9315625ccbc", ExitCode = 0, Result = "0.76" },
+            new JobResult { JobId = 1, ImageA = "0deae28a26727ebe30ecf2896e5862f1", ImageB = "30f811776cb26a19a693b6dcaa165a30", ExitCode = 0, Result = "0.35" }
+        };
+
+        public IEnumerable<JobResult> GetAllProducts()
+        {
+            return placeholderResults;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var result = placeholderResults.FirstOrDefault((p) => p.JobId == id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+    }
+}
---
+++ JobServer/JobServer/Models/JobResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JobServer.Models
+{
+    public class JobResult
+    {
+        public int JobId { get; set; }
+        public string ImageA { get; set; }
+        public string ImageB { get; set; }
+        public int ExitCode { get; set; }
+        public string Result { get; set; }
+    }
+}
