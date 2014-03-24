944b43e519e4f62cab762f782dd9d665487bac01
George Field <gf12815@my.bristol.ac.uk>
2014-03-24 01:19:45 +0000

    Add very basic sample executable for job server.

---
+++ TestExecutable/TestExecutable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestExecutable
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.Error.WriteLine("Invalid Arguments");
+                return 1;
+            }
+            else
+            {
+                Console.WriteLine(0.75);
+                return 0;
+            }
+        }
+    }
+}
