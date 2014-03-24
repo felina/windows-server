1cce5731e6c000a86b444a20cf5a99cbec38989f
Andrew Stuart <narayn60@gmail.com>
2014-03-24 00:54:39 +0000

    Added Job Controller

---
+++ JobServer/JobServer/Controllers/JobController.cs
@@ -0,0 +1,44 @@
+﻿using JobServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace JobServer.Controllers
+{
+    public class JobController : ApiController
+    {
+        // GET api/job
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/job/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/job
+        public string Post([FromBody]Job value)
+        {
+            Debug.WriteLine("Being posted to");
+            Debug.WriteLine(value.ImageA);
+            return value.ImageB;
+        }
+
+        // PUT api/job/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/job/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ JobServer/JobServer/Models/Job.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JobServer.Models
+{
+    public class Job
+    {
+        public int JobId { get; set; }
+        public string ImageA { get; set; }
+        public string ImageB { get; set; }
+    }
+}
