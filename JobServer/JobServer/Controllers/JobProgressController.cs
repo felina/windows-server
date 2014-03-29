fbe95202e83c12ea3e9a50f5c919d96b8d112150
Alistair <alistair.wk@gmail.com>
2014-03-29 17:37:16 +0000

    Added basic JobProgress controller

---
+++ JobServer/JobServer/Controllers/JobProgressController.cs
@@ -0,0 +1,28 @@
+﻿using JobServer.Executables;
+using JobServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace JobServer.Controllers
+{
+    public class JobProgressController : ApiController
+    {
+        // GET api/jobprogress/id
+        public JobProgress Get(int id)
+        {
+            if (ProcessManager.Jobs.ContainsKey(id))
+            {
+                return JobProgress.CreateFromStored(ProcessManager.Jobs[id]);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ JobServer/JobServer/Models/JobProgress.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using JobServer.Executables;
+
+namespace JobServer.Models
+{
+    public class JobProgress
+    {
+        public int JobId { get; set; }
+        public bool Started { get; set; }
+        public bool Completed { get; set; }
+        public float Progress { get; set; }
+
+        public static JobProgress CreateFromStored(StoredJob job)
+        {
+            JobProgress progress = new JobProgress();
+            progress.Completed = job.Completed;
+            progress.Started = job.Started;
+            progress.Progress = (float)job.Images.Length / (float)job.BatchIndex;
+
+            return progress;
+        }
+    }
+}
