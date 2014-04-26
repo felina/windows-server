52766f4d381f7f7ca9b32dc8560d8abc24bd6cd2
Alistair <alistair.wk@gmail.com>
2014-04-26 18:19:16 +0100

    ProcessManager docs + job deletion

---
+++ JobServer/JobServer/Controllers/DeleteJobController.cs
@@ -0,0 +1,33 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using JobServer.Executables;
+using JobServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using JobServer.App_Code;
+using System.Web;
+using System.IO;
+
+namespace JobServer.Controllers
+{
+    public class DeleteJobController : ApiController
+    {
+        // api/deletejob/id
+        public IHttpActionResult Get(int id)
+        {
+            if (ProcessManager.RemoveJob(id))
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest("Job in progress or non-existent");
+            }
+        }
+    }
+}
--- JobServer/JobServer/Executables/ProcessManager.cs
+++ JobServer/JobServer/Executables/ProcessManager.cs
@@ -19,7 +19,20 @@ namespace JobServer.Executables
         /// </summary>
         private static Dictionary<int, StoredJob> Jobs = new Dictionary<int, StoredJob>();
 
-        // Adds a new job
+        /// <summary>
+        /// Utility function
+        /// </summary>
+        /// <param name="id">ID of job</param>
+        /// <returns>The path to the given job</returns>
+        private static string JobPath(int id)
+        {
+            return HttpRuntime.AppDomainAppPath + "/App_Data/Jobs/" + id;
+        }
+
+        /// <summary>
+        /// Adds a new job - to both the cache and application memory
+        /// </summary>
+        /// <param name="job">Job Model object to store</param>
         public static void AddJob(Job job)
         {
             if (JobLoaded(job.JobId))
@@ -37,7 +50,54 @@ namespace JobServer.Executables
             }
         }
 
-        // Returns a stored job
+        /// <summary>
+        /// Removes a job from memory and the cache. Aborts if the job is running, or if it isn't cached.
+        /// </summary>
+        /// <param name="id">ID of job to remove</param>
+        /// <returns>Whether the job was removed</returns>
+        public static bool RemoveJob(int id)
+        {
+            bool cached = JobCached(id);
+
+            if (!cached)
+            {
+                Debug.WriteLine("Warning: Attempted to remove job which is not in the cache (" + id + ")");
+                return false;
+            }
+
+            // Remove from memory
+            if (JobLoaded(id))
+            {
+                StoredJob job = GetJob(id);
+
+                // Check it's not running
+                if (job.Started && !job.Completed)
+                {
+                    return false;
+                }
+
+                Jobs.Remove(id);
+            }
+
+            // Remove from cache
+            try
+            {
+                Directory.Delete(JobPath(id), true);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Retrieves a stored job. Loads the job from the cache if not in memory already.
+        /// </summary>
+        /// <param name="id">ID of job to load</param>
+        /// <returns>The StoredJob object for the job</returns>
         public static StoredJob GetJob(int id)
         {
             if (JobLoaded(id))
@@ -87,21 +147,32 @@ namespace JobServer.Executables
             }
         }
 
-        // Tests if a job is loaded into memory
+        /// <summary>
+        /// Tests if a job is loaded into memory
+        /// </summary>
+        /// <param name="id">Job ID</param>
+        /// <returns>Whether the job exists in the Job dictionary</returns>
         public static bool JobLoaded(int id)
         {
             return Jobs.ContainsKey(id);
         }
 
-        // Tests whether a job of given id is already stored 
+        /// <summary>
+        /// Tests whether a job is cached on the hard drive, independent of whether it is loaded in memory
+        /// </summary>
+        /// <param name="id">Job ID</param>
+        /// <returns>Whether the job exists on the hard drive</returns>
         public static bool JobCached(int id)
         {
             // Checks server hard drive for the executable
             return System.IO.Directory.Exists(HttpContext.Current.Server.MapPath("~/App_Data/Jobs/"+id));
         }
 
-
-        //Validates the image to make sure it's an MD-5 Hash
+        /// <summary>
+        /// Checks the image hash is valid MD-5
+        /// </summary>
+        /// <param name="Image">String Image hash</param>
+        /// <returns></returns>
         static bool ValidateImageName(String Image)
         {
             if (Image.Length == 32)
@@ -116,7 +187,11 @@ namespace JobServer.Executables
             return false;
         }
 
-        // Launches the application on the command line and saves the results
+        /// <summary>
+        /// Launches the Job's executable on the cammand line and saves the results in memory
+        /// </summary>
+        /// <param name="fileName">Name of the executable in the Job's .zip</param>
+        /// <param name="jobId">Job ID</param>
         public static void RunJob(string fileName, int jobId)
         {
             
