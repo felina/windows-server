d74490f55604b9c1a27eb527462dab832da2c967
Andrew Stuart <narayn60@gmail.com>
2014-04-28 13:46:40 +0100

    Empty function definitions added

--- JobServer/JobServer/Controllers/JobController.cs
+++ JobServer/JobServer/Controllers/JobController.cs
@@ -37,5 +37,51 @@ namespace JobServer.Controllers
         }
 
         // TODO: Job control - pause, halt etc
+
+        // Pauses the specified job
+        public string Pause(int id)
+        {
+            StoredJob job = ProcessManager.GetJob(id);
+            if (job == null)
+            {
+                return "Job not stored";
+            }
+            if (job.Completed)
+            {
+                return "Job already completed";
+            }
+            else if (!job.Started)
+            {
+                return "Job not started";
+            }
+            else
+            {
+                Action c = delegate { }; //Pause the job here, if already paused do nothing
+                c();
+                return "Job Paused";
+            }
+        }
+
+        // Stops the specified job
+        public string Stop(int id)
+        {
+            StoredJob job = ProcessManager.GetJob(id);
+            if (job == null)
+            {
+                return "Job not stored";
+            }
+            if (job.Started) {
+                Action d = delegate {}; //Stop the job here
+                d();
+                return "Job Stopped";
+            }
+            else
+            {
+                return "Job Stopped":
+            }
+        }
+
+
+
     }
 }
